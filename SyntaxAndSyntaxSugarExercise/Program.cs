0e39a0960b74f84866721ed2b483327517c4e753
Kayla Knelle <kaynell3234@gmail.com>
2025-07-29 11:59:07 -0400

    Completed the exercise and included the three different things, inferred typing, string interpolation, and ternary operator.

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.ComponentModel.Design;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*var answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + "is less than 9";
+            }
+            else
+            {
+                response = answer + "is greater than 9";
+            } */
+            
+            //if else statement
+            //with inferred typing
+
+            var answer = 4;
+            
+
+            if (answer < 9)
+            {
+                Console.WriteLine($"{answer} is less than 9");
+            }
+            else
+            {
+                Console.WriteLine($"{answer} is greater than 9");
+            } 
+            //ternary operator
+            
+            Console.WriteLine(answer < 9 ? "Your answer is less than 9" : answer > 15 ?"Your answer is greater than 15": "Your answer is between 9 and 15");
+            
+            //string interpolation
+            Console.WriteLine(answer > 9 ? $"{answer} is greater than 9"  : $"{answer} is less than 9");
+            
         }
+        
     }
 }
